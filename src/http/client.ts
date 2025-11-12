85e92855c54ba0a4ba454538fc54550d5b5ee2ce
Manish Karki <65812794+Manish-Karke@users.noreply.github.com>
2025-11-12 15:52:16 +0545

    Update baseURL to include '/api' path

--- src/http/client.ts
+++ src/http/client.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseURL ="https://my-book-store-b63l.vercel.app"
+const baseURL ="https://my-book-store-b63l.vercel.app/api"
 export const api = axios.create({ baseURL, withCredentials: true, 
                                  headers: { "Content-Type": "application/json",
                                            Accept: "application/json", 
